536e8730cbe19b790136d6ac35d98eb378c060fc
Andrzej Kapczyński <75850182+Endrju00@users.noreply.github.com>
2021-10-07 19:04:45 +0200

    Plotting one algorithm

--- lab1/wykresy.py
+++ lab1/wykresy.py
@@ -1,12 +1,35 @@
+import csv
 import matplotlib.pyplot as plt
 
 
+def plot_data(path):
+    '''Takes in path to the csv file, returns the dictionary with x and y axises data.'''
+
+    with open(path) as csv_file:
+        data = {
+            'x': [],
+            'y': []
+        }
+
+        reader = csv.reader(csv_file)
+        counter = 0
+        for row in reader:
+            counter += 1
+
+            if counter == 1:
+                continue
+            
+            data['x'].append(int(row[1]))
+            runs = list(map(float, row[2:]))
+            data['y'].append(round(sum(runs)/32, 4))
+    
+    return data
+
 def main():
-    plt.figure(figsize=(3, 3))
-    plt.plot([100, 200, 300, 400], [0.1, 0.2, 0.8, 0.9])
-    # plt.savefig('myplot.pdf')
+    a = plot_data('data/cel.csv')
+
+    plt.plot(a['x'], a['y'])
     plt.show()
-    plt.close()
 
 
 if __name__ == '__main__':
